7345268df49fbb086fca76ac110a2fa9883b40b0
dobbse42 <49924950+dobbse42@users.noreply.github.com>
2023-12-25 17:05:43 -0600

    Update merry.py

--- merry.py
+++ merry.py
@@ -4,7 +4,7 @@ import nltk
 nltk.download('punkt')
 
 def be_merry():
-  paper = next(arxiv.Client().results(arxiv.Search(id_list = ["2107.09200"])))
+#  paper = next(arxiv.Client().results(arxiv.Search(id_list = ["2107.09200"])))
 #  my_filename = paper.get_short_id() + '_' + paper.title.replace(' ', '_') + '.pdf'
 #  return my_filename
   return "be merry"
