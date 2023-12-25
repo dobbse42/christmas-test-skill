70266d397fdbcbeaf8c32d02d3ca2b068eb70ba9
dobbse42 <49924950+dobbse42@users.noreply.github.com>
2023-12-25 16:09:58 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler
 
+from .merry import be_merry
+
 
 class ChristmasTest(MycroftSkill):
     def __init__(self):
@@ -8,6 +10,8 @@ class ChristmasTest(MycroftSkill):
     @intent_file_handler('test.christmas.intent')
     def handle_test_christmas(self, message):
         self.speak_dialog('test.christmas')
+        my_msg = be_merry()
+        self.speak(my_msg)
 
 
 def create_skill():
