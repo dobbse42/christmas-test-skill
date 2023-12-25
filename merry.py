5ee0145dfa74752a884e06641db5d175eb9e3bf3
dobbse42 <49924950+dobbse42@users.noreply.github.com>
2023-12-25 16:35:31 -0600

    Update merry.py

--- merry.py
+++ merry.py
@@ -4,8 +4,8 @@ import arxiv
 def be_merry():
   paper = next(arxiv.Client().results(arxiv.Search(id_list = ["2107.09200"])))
   my_filename = paper.get_short_id() + '_' + paper.title.replace(' ', '_') + '.pdf'
-  return my_filename
-#  return "be merry"
+#  return my_filename
+  return "be merry"
 
 
 def nth_day():
