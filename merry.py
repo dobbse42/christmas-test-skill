da274fdbc63f3c037ae4ae594f79cb8c6016bfed
dobbse42 <49924950+dobbse42@users.noreply.github.com>
2023-12-25 16:34:48 -0600

    Update merry.py

--- merry.py
+++ merry.py
@@ -1,7 +1,12 @@
 import numpy as np
+import arxiv
 
 def be_merry():
-  return "be merry"
+  paper = next(arxiv.Client().results(arxiv.Search(id_list = ["2107.09200"])))
+  my_filename = paper.get_short_id() + '_' + paper.title.replace(' ', '_') + '.pdf'
+  return my_filename
+#  return "be merry"
+
 
 def nth_day():
   my_arr = np.ones(12)
