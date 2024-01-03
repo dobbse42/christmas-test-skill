9d433c4c277fd9290b89b22a7f90df8d7dd83b1c
dobbse42 <edjunk13@hotmail.com>
2024-01-03 03:04:18 +0200

    removed duplicated functions in merry.py

--- merry.py
+++ merry.py
@@ -14,22 +14,7 @@ def be_merry():
 #  data = urllib.request.urlopen(url)
 #  print(data.read().decode('utf-8'))
   id = "1904.12956"
-  feed = get_feed(id)
-  pdf = get_pdf(feed)
   return "done!"
-
-def get_feed(id):
-  url = 'http://export.arxiv.org/api/query?id_list=' + str(id)
-  data = requests.get(url)
-  return data.text
-
-def get_pdf(my_xml):
-  feed = feedparser.parse(my_xml)
-  for link in feed['entries'][0]['links']:
-    if link['rel'] == 'related' and link['title'] == 'pdf':
-      pdf_url = link['href']
-  pdf = requests.get(pdf_url)
-  return pdf.content
 
 def nth_day():
   my_arr = np.ones(12)
